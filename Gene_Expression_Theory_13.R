1c76cd4772eb27d4ac265a767d18e548ea13e794
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-02-25 07:28:45 -0700

    Update Gene_Expression_Theory_13.R

--- Gene_Expression_Theory_13.R
+++ Gene_Expression_Theory_13.R
@@ -323,3 +323,24 @@ Reference.3<-c("Sievert, C. and Shirley, K. (2014)",
 
 
 #----------------------------------------Function Library-----------------------------------
+#-------------Function Template Library for Classroom Presentation and Modification---------------------
+f.1<-function(X)
+ {
+  Z<-""
+  a<-1
+  W<-runif(length(X),0,1)
+  for(i in 1:length(X))
+  {  
+	Z<-stringr::str_c(Z,X[i])
+	W[i]<-a*W[i]
+  }
+  output<-list()
+  output$X<-X
+  output$a<-a
+  output$Z<-Z
+  output$W<-W
+  return(output)
+ } 
+test.f.1<-f.1(letters)
+test.f.1
+
