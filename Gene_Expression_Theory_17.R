14a3529b76d36a283cb7f7367b88bfebe2841504
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-02-25 12:51:30 -0700

    Update Gene_Expression_Theory_17.R

--- Gene_Expression_Theory_17.R
+++ Gene_Expression_Theory_17.R
@@ -17,9 +17,72 @@ selectedOncogenes <- c("ABL1", "ALK", "BRAF", "CCND1", "CCND3", "CCNE1", "CCNE2"
                        "WNT5B", "WNT10A", "WNT11", "WNT2", "WNT1", "WNT7B", "WISP1", 
                        "WNT8B", "WNT7A", "WNT16", "WISP2", "WISP3", "FZD5", "FZD1")
 #---------------------------------------Function Library------------------------------------
+f.1<-function(gene.1,gene.2,Visualization=FALSE)
+{
+  acceptablePlotTypes <- c("drug", "cop", "exp", "xai", "exo", "mut", "mir", "pro", "mda")
+  copPrefix <- "cop"
+  expPrefix <- "exp"
+  xaiPrefix <- "xai"
+  exoPrefix <- "exo"
+  mutPrefix <- "mut"
+  mirPrefix <- "mir"
+  proPrefix <- "pro"
+  #------Filters------
+  copData <- molData[[copPrefix]]
+  expData <- molData[[expPrefix]]
+  xaiData <- molData[[xaiPrefix]]
+  exoData <- molData[[exoPrefix]]
+  mutData <- molData[[mutPrefix]]
+  mirData <- molData[[mirPrefix]]
+  proData <- molData[[proPrefix]]
+  
+  gene.1<-gene.1
+  gene.2<-gene.2
+  gene<-c(gene.1,gene.2)
+  
+  # Get the cell lines names for cell lines meeting particular thresholds
+  copKnockdown <- names(which(molData[["cop"]][paste0("cop", gene), ] < -1))
+  expKnockdown <- names(which(molData[["exp"]][paste0("exp", gene), ] < -1.5))
+  mutKnockdown <- names(which(molData[["mut"]][paste0("mut", gene), ] == 1))
+  
+  # Make composite pattern
+  pattern <- rep(0, length(molData[["cop"]][paste0("cop", gene), ]))
+  names(pattern) <- names(molData[["cop"]][paste0("cop", gene), ])
+  tmp <- Reduce(union, list(copKnockdown, expKnockdown, mutKnockdown))
+  pattern[tmp] <- 1
+  
+  #-----Summarize-----
+  Table.1<-NULL
+  
+  #-----Visualize-----
+  if(Visualization)
+  {
+    #------------------Profile Visualization------
+    plots <- c("exp","exp") 
+    plotCellMiner(drugAct, molData, plots, NULL, gene)
+    # Composite plot data
+    extraPlot <- list()
+    extraPlot[["title"]] <- "Composite Pattern"
+    extraPlot[["label"]] <- "Knockdown Composite (Binary)"
+    extraPlot[["values"]] <- pattern
+    
+    plotCellMiner(molData=molData, plots=c("cop", "exp", "mut"), gene=gene, extraPlot=extraPlot)
+    
+  }
+  
+  #Object Model
+  output<-list()
+  output$Genes<-gene
+  output$Table.1<-Table.1
+  return(output)
+  
+  
+}
+test.f.1<-f.1("TP53","MDM2",Visualization=TRUE)
+test.f.1
 
 #-------------Function Template Library for Classroom Presentation and Modification---------------------
-f.1<-function(X)
+f.2<-function(X)
  {
   Z<-""
   a<-1
@@ -36,5 +99,5 @@ f.1<-function(X)
   output$W<-W
   return(output)
  } 
-test.f.1<-f.1(letters)
-test.f.1
+test.f.2<-f.2(letters)
+test.f.2
