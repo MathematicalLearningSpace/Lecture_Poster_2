cd14401a7da0a962dcb1443ca31b3cbcde976b24
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-02-26 16:40:13 -0700

    Update Normal_Mode_Theory_5.R

--- Normal_Mode_Theory_5.R
+++ Normal_Mode_Theory_5.R
@@ -1,7 +1,7 @@
 #----------------------------------------------R API ------------------------------------------------------
 library(bio3d);library(xtable);library(Peptides);library(stringi);library(stringr);library(xtable);library(readr);library(readxl)
 #---------------------------------------------Data From String DB and GeneCards---------------------------------------------
-
+#---------------Formatted Student Notes for Lectures------------------------------
 WD64.Enrichment.Process <- as.data.frame(read_delim("enrichment.Process.tsv", "\t", escape_double = FALSE, trim_ws = TRUE))
 
 WD64.Enrichment.Process$`#pathway ID`
@@ -17,12 +17,14 @@ immune.response_regulating.cell.surface.receptor.signaling.pathway<-WD64.Enrichm
 immune.response_regulating.cell.surface.receptor.signaling.pathway.network<-WD64.Enrichment.Process$`matching proteins in your network (labels)`[4]
 
 D6RCR1.Human.pdb<-read.pdb("D6RCR1_HUMAN_1.pdb")
+
 Proteins.Study<-c(immune.response_regulating.cell.surface.receptor.signaling.pathway.network)
 
-#-------------------------------------------------------------Tables-------------------------------------------
+#------------------------------Tables-------------------------------------------
 
 Table.1<-xtable(WDR64.Protein.Annotations.Notes)
 Table.2<-xtable(WD64.Enrichment.Process)
+
 #----------------------Figures to be added by students------------------------------------------
 
 
