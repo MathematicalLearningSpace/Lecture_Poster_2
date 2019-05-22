de6439b7543758d4dfba739bca94952c9a7dd8b2
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-05-22 06:11:19 -0700

    Add files via upload

---
+++ Normal_Mode_Theory_6.R
@@ -0,0 +1,4 @@
+#----------------------------------------------------R API------------------------------------
+library(gdata);library(bio3d);library(igraph);library(sna);library(ips);library(phangorn);library(proteomics)
+library(dcGOR);library(MDplot);library(UniProt.ws);library(circlize);library(BioPhysConnectoR);library(protr)
+library(seqinr);library(Biostrings);library(Peptides);library(PearsonDS);library(xtable)
