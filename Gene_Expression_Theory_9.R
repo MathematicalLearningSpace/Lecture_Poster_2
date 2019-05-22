6a19e57dc7112cc49dac95fbeb6bce6883bdc7e4
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-05-22 06:06:10 -0700

    Add files via upload

---
+++ Gene_Expression_Theory_9.R
@@ -0,0 +1,4 @@
+#-----------------------------R Code To Modify in the Classroom Lecture with Students-----------------------
+#-----------------------------------R API ----------------------------------------------------
+library(fractal); library(fracdiff);library(timeSeries);library(RandomFields);library(boot);library(vars)
+library(urca);library(PearsonDS);library(stringr);library(stringi);library(corrplot);library(ggplot2);library(xtable)
