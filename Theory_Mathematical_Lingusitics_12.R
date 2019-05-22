e755a67747c3c15d124019b0e40397da24518867
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-05-22 06:15:46 -0700

    Add files via upload

---
+++ Theory_Mathematical_Lingusitics_12.R
@@ -0,0 +1,4 @@
+#-----------------------------R Code To Modify in the Classroom Lecture with Students-----------------------
+#------------------------------------------------R API ----------------------------------------------------------
+library(tm);library(stringi);library(stringr);library(utils);library(CHNOSZ);library(PearsonDS);library(xtable)
+library(bio3d);library(Peptides);library(jsonlite);library(rjson);library(xtable);library(seqinr)
